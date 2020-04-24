a67eeba022a24358bd948e2249070869b9fa2cc0
Adrienne <52835792+ItsJustAco@users.noreply.github.com>
2020-04-24 13:59:44 -0700

    Added Fungus Extension

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class Plant : MonoBehaviour
 {
+    // Set associated flowchart to plant
+    public Flowchart flowchart;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,7 @@ public class Plant : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     private void ShowMeun()
@@ -23,10 +26,14 @@ public class Plant : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //If player is touching/in front of plant, and presses space, then start plant flowchart
         if (collision.gameObject.tag == "Player")
         {
             ShowMeun();
             Debug.Log("Touch");
+           
+            //flowchart.ExecuteBlock("Start");
+            
         }
     }
 }
---
+++ Assets/playerInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class playerInteraction : MonoBehaviour
+{
+    // Set associated flowchart to plant
+    public Flowchart flowchart;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If player is touching/in front of plant, and presses space, then play plant flowchart
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            flowchart.ExecuteBlock("Start");
+        }
+
+    }
+}
