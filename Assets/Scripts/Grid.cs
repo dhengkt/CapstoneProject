32abbda1e7d3cda7b91a8a028025080570ac2913
Katie <dhengkt@icloud.com>
2020-04-25 18:03:12 -0700

    some changes in Grid.cs

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -2,8 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid { 
+public class Grid
+{
 
+    private int width;
+    private int height;
+    private int[,] gridArray;
 
+    // constructor
+    public Grid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+
+        gridArray = new int[width, height];
+    }
 
 }
