e8a0c90df9895d0a7f14ca0fa7ff44066ecc498b
Katie <dhengkt@icloud.com>
2020-04-26 23:02:50 -0700

    imported codemonkey package and created test file for grid map

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,21 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Grid
 {
 
     private int width;
     private int height;
+    private float cellSize;
     private int[,] gridArray;
 
     // constructor
-    public Grid(int width, int height)
+    public Grid(int width, int height, float cellSize)
     {
         this.width = width;
         this.height = height;
+        this.cellSize = cellSize;
 
         gridArray = new int[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
+            }
+        }
+    }
+
+    // convert x and y to world position
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
     }
 
 }
---
+++ Assets/Scripts/GridMapTesting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMapTesting : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid grid = new Grid(4, 2, 10f);
+    }
+}
