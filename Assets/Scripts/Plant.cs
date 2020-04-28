cae54dbae3e5e16ed8fa8bb55b738cfd94d6094d
Adrienne <52835792+ItsJustAco@users.noreply.github.com>
2020-04-27 22:33:48 -0700

    Flowchart connected to plant

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -7,6 +7,7 @@ public class Plant : MonoBehaviour
 {
     // Set associated flowchart to plant
     public Flowchart flowchart;
+    private bool plantTrigger;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,7 @@ public class Plant : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 
     private void ShowMeun()
@@ -26,14 +27,22 @@ public class Plant : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //If player is touching/in front of plant, and presses space, then start plant flowchart
         if (collision.gameObject.tag == "Player")
         {
             ShowMeun();
             Debug.Log("Touch");
-           
-            //flowchart.ExecuteBlock("Start");
-            
         }
     }
+    //if player is on the square in front of a plant and presses x, trigger that plant's dialogue
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Staying");
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Talk");
+            flowchart.ExecuteBlock("Start");
+           
+        }
+    }
+
 }
