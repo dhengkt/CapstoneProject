4f35cbc180edfc976c3f84dd073fec68813bf448
Katie <dhengkt@icloud.com>
2020-04-27 21:22:31 -0700

    made some changes in Grid.cs and GridMapTesting.cs

--- Assets/Scripts/GridMapTesting.cs
+++ Assets/Scripts/GridMapTesting.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class GridMapTesting : MonoBehaviour
 {
+    private Grid grid;
+
     private void Start()
     {
         Grid grid = new Grid(4, 2, 10f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
     }
 }
