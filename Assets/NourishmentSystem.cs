418bfa8893a9be09acf87cbae129cee314c0d0d0
dhengkt <49250200+dhengkt@users.noreply.github.com>
2020-04-30 20:42:34 -0700

    implemented basic plant nourishment system

--- Assets/NourishmentSystem.cs
+++ Assets/NourishmentSystem.cs
@@ -3,17 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NourishmentSystem : MonoBehaviour
-{
-    private SpriteRenderer rend;
-    private Sprite stage1, stage2, stage3, stage4;
+{   
+    private enum State
+    {
+        Stage1,
+        Stage2,
+        Stage3,
+        Stage4,
+    }
+
+    [SerializeField] Sprite stage1;
+    [SerializeField] Sprite stage2;
+    [SerializeField] Sprite stage3;
+    [SerializeField] Sprite stage4;
+    private State currState;
+
+    private void Awake()
+    {
+        currState = State.Stage1;
+    }
 
     private void Start()
     {
-        rend = GetComponent<SpriteRenderer>();
-        stage1 = Resources.Load<Sprite>("PlantA S1");
-        stage2 = Resources.Load<Sprite>("PlantA S2");
-        stage3 = Resources.Load<Sprite>("PlantA S3");
-        stage4 = Resources.Load<Sprite>("PlantA S4");
-        rend.sprite = stage2;
+        
     }
+
+    private void Update()
+    {
+        // get SpriteRenderer to change sprite in different stage
+        SpriteRenderer rend = gameObject.GetComponent<SpriteRenderer>();
+
+        switch (currState)
+        {
+            default:
+            case State.Stage1:
+                rend.sprite = stage1;
+                // if water/fertilizer > 2, go to stage 2 and give another story to player
+                break;
+            case State.Stage2:
+                rend.sprite = stage2;
+                // if water/fertilizer > 4, go to stage 2 and give another story to player
+                break;
+            case State.Stage3:
+                rend.sprite = stage3;
+                // if water/fertilizer > 6, go to stage 2 and give another story to player
+                break;
+            case State.Stage4:
+                rend.sprite = stage4;
+                // if water/fertilizer > 10, go to stage 2 and give another story to player
+                break;
+        }
+    }
+
+    private void AddWater()
+    {
+
+    }
+
 }
