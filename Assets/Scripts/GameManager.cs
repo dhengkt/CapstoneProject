aaacdeee37b6179294114c85716f70f3a69c80b0
dhengkt <49250200+dhengkt@users.noreply.github.com>
2020-05-05 02:59:08 -0700

    Added Game Manager into scene and implemented creating first plant in scene
    First plant will be created once the game start.
    Still need to figure out how to assign the flowchart to the plant create when the game start.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Fungus;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,12 +11,15 @@ public class GameManager: MonoBehaviour
     int[] plantLocations = new int[6] { 0, 0, 0, 0, 0, 0 }; // 0: Vacant; 1: Occupied
     Plant[] currentPlants = new Plant[6]; // Keeps track of all plants currently in the game
     int numberOfPlants = 0;
+    private GameObject firstPlant = null; // First plant in the game
+    private Flowchart temFlowchart = null; // for assign flowchat to first plant
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        firstPlant = Resources.Load<GameObject>("Prefabs/Plant");
+       CreateFirstPlant();
     }
 
     // Update is called once per frame
@@ -27,24 +32,38 @@ public class GameManager: MonoBehaviour
         }
     }
 
+    /**
+     * 
+     * Need to find a way to create the flowchart object and add it to the plant create in this function
+     * 
+     **/
+    private void CreateFirstPlant()
+    {
+        GameObject p = Instantiate(firstPlant);
+        float x = 5.81f;
+        float y = -1.38f;
+        p.transform.position = new Vector3(x, y, -5f);
+        numberOfPlants++;
+        assignStory(p);
+    }
+
     // Create a plant object before placing it in the game
     public void createPlant()
     {
         // Create and add plant to currentPlants[]
         Debug.Log("Creating Plant...");
-        currentPlants[numberOfPlants] = new Plant(assignStory(), assignOpenLocation());
+        //currentPlants[numberOfPlants] = new Plant(assignStory(), assignOpenLocation());
         Debug.Log(currentPlants[numberOfPlants]);
         numberOfPlants += 1;
         Debug.Log("Unused Stories (after assign): " + unusedStories);
     }
-    private int assignStory()
+    private int assignStory(GameObject plant)
     {
         Debug.Log("Unused Stories: " + unusedStories);
-        Random rnd = new Random();
-        int storyIndex = Random.Range(0,unusedStories.Count);
+        UnityEngine.Random rnd = new UnityEngine.Random();
+        int storyIndex = UnityEngine.Random.Range(0,unusedStories.Count);
         int tempStory = unusedStories[storyIndex];
         unusedStories.Remove(storyIndex);
-
         return 0;
     }
     private int assignOpenLocation()
