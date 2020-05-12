2d12bb628cd1f3a7071830220c2244a8c33fba92
Katie <dhengkt@icloud.com>
2020-05-12 04:25:19 -0700

    Created Tutorial scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Tutoiral()
+    {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
     public void PlayGame ()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
 	}
 
 	public void QuickGame()
