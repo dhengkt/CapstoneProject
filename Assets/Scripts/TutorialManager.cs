18a0228b4e6df91853bc5cf6c7e9f0374135bc1c
dhengkt <49250200+dhengkt@users.noreply.github.com>
2020-05-13 00:34:23 -0700

    Updated tutorial script

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using Fungus;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TimeSystem tSystem;
+    private PlayerActions pActions;
+    private Flowchart tuFlowchart;
+    // Tutorial Scene:
+    // When game starts up, tutorial plant will spawn and tutorial dialogue will appear
+    // Player talks to the tutorial plant and gives it water
+    // Player finishes the reading the rest of the tutorial plant
+    // Player leaves greenhouse and finds a new plant that replaces the tutorial plant
+
+    void Awake()
     {
-        
+        GameObject temDoor = Resources.Load<GameObject>("Prefabs/Door");
+        GameObject door = Instantiate(temDoor);
+
+        door.transform.position = new Vector3(-.23f, -7.99f, 0f);
+        tSystem = door.GetComponent<TimeSystem>();
+        pActions = door.GetComponent<PlayerActions>();
+
+        tuFlowchart = FindObjectOfType<Flowchart>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        CreateTutorialPlant();
+    }
+
     void Update()
     {
-        
+
+    }
+
+    private void CreateTutorialPlant()
+    {
+        Plant plant;
+        GameObject p = Resources.Load<GameObject>("Prefabs/Plant");
+        GameObject plantObject = Instantiate(p);
+        plant = plantObject.GetComponent<Plant>();
+        plant.transform.position = new Vector3(5.81f, -1.38f, -5f);
+
+        plant.flowchart = tuFlowchart;
     }
 }
