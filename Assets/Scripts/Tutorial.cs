b963bbbe87082e0140c5e6a1f8144187acee5088
dhengkt <49250200+dhengkt@users.noreply.github.com>
2020-05-16 00:45:30 -0700

    renamed TutoiralManager.cs to Tutorial.cs

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialManager : MonoBehaviour
+public class Tutorial : MonoBehaviour
 {
     private TimeSystem tSystem;
     private ActionsMenu pActions;
