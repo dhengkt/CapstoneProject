e63a0ae566ac5081b328c1565aeabffd2eccf583
dhengkt <49250200+dhengkt@users.noreply.github.com>
2020-05-26 04:44:29 -0700

    created pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPause = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPause = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPause = true;
+    }
+}
