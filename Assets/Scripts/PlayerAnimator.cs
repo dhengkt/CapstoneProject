c348b34ea0ef616c20e2dd09277c8b240836617e
dhengkt <49250200+dhengkt@users.noreply.github.com>
2020-05-29 01:56:55 -0700

    adding water/fertilizer animation will only play when player press buttons and water/fertilizer amount is bigger than 0

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    public GameObject player;
+    //public GameObject player;
+    public Player player;
     public Animator animator;
     private bool onPlant;
 
     void Update()
     {
         animator.SetBool("OnPlant", onPlant);
-        if (Input.GetButtonDown("Water"))
+        if (Input.GetButtonDown("Water") && player.wAmount > 0)
         {
             player.GetComponent<Animator>().Play("Player_Watering");
         }
-        if (Input.GetButtonDown("Fertilize"))
+        if (Input.GetButtonDown("Fertilize") && player.fAmount > 0)
         {
             player.GetComponent<Animator>().Play("Player_Fertilize");
         }
